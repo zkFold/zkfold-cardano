29359658addb830fd90a5d8e67e5ef10f9a0ed42
zlonast <zlonast3@gmail.com>
2024-07-14 10:21:53 +0000

    add bash strict mode

--- e2e-test/plonk/01-init-transaction.sh
+++ e2e-test/plonk/01-init-transaction.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+set -e
+# Unofficial bash strict mode.
+# See: http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -u
+set -o pipefail
+
 keypath=./keys
 assets=../assets
 
