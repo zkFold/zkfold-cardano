9a828ac539003e6b8019e56ca0b85dccaf0094d9
charrsky <maxchrlv@gmail.com>
2024-08-14 14:40:45 +0300

    redo convertG1/2 functions and add tracing

--- src/ZkFold/Cardano/Plonk/OffChain.hs
+++ src/ZkFold/Cardano/Plonk/OffChain.hs
@@ -5,6 +5,7 @@
 module ZkFold.Cardano.Plonk.OffChain where
 
 import           Data.Aeson                                  (FromJSON, ToJSON)
+import           Data.Binary
 import qualified Data.Vector                                 as V
 import           GHC.ByteOrder                               (ByteOrder (..))
 import           GHC.Generics                                (Generic)
@@ -18,12 +19,15 @@ import           ZkFold.Base.Algebra.Basic.Class
 import           ZkFold.Base.Algebra.Basic.Field             (Ext2 (..), Zp, fromZp, toZp)
 import           ZkFold.Base.Algebra.Basic.Number            (KnownNat, value)
 import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1, BLS12_381_G2, Fr)
-import           ZkFold.Base.Algebra.EllipticCurve.Class     (Point (..), EllipticCurve (..))
+import           ZkFold.Base.Algebra.EllipticCurve.Class     (Point (..), EllipticCurve (..), compress)
 import           ZkFold.Base.Protocol.ARK.Plonk
 import           ZkFold.Base.Protocol.NonInteractiveProof    (FromTranscript (..), NonInteractiveProof (..), ToTranscript (..))
 import           ZkFold.Cardano.Plonk.OnChain.BLS12_381.F    (F (..))
 import           ZkFold.Cardano.Plonk.OnChain.BLS12_381.G1   (G1)
 import           ZkFold.Cardano.Plonk.OnChain.Data           (InputBytes (..), ProofBytes (..), SetupBytes (..))
+import           Data.ByteString.Lazy (toStrict)
+import           Debug.Trace (trace)
+import qualified Data.ByteArray as BA
 
 --------------- Transform Plonk Base to Plonk BuiltinByteString ----------------
 
@@ -36,9 +40,9 @@ mkSetup (PlonkSetupParamsVerify {..}, PlonkCircuitCommitments {..}) = SetupBytes
   , g0'   = convertG1 gen
   , h0'   = convertG2 gen
   , x2'   = convertG2 x2''
-  , omega = F $ convertF omega''
-  , k1    = F $ convertF k1''
-  , k2    = F $ convertF k2''
+  , omega = F $ convertZp omega''
+  , k1    = F $ convertZp k1''
+  , k2    = F $ convertZp k2''
   , cmQl' = convertG1 cmQl
   , cmQr' = convertG1 cmQr
   , cmQo' = convertG1 cmQo
@@ -50,7 +54,7 @@ mkSetup (PlonkSetupParamsVerify {..}, PlonkCircuitCommitments {..}) = SetupBytes
   }
 
 mkInput :: Input (PlonkN n) -> InputBytes
-mkInput (PlonkInput input) = InputBytes . F . convertF $ V.head input
+mkInput (PlonkInput input) = InputBytes . F . convertZp $ V.head input
 
 mkProof :: forall n. KnownNat n => SetupBytes -> Proof (PlonkN n) -> ProofBytes
 mkProof setup' proof@(PlonkProof cmA cmB cmC cmZ cmT1 cmT2 cmT3 proof1 proof2 a_xi b_xi c_xi s1_xi s2_xi z_xi) = ProofBytes
@@ -63,12 +67,12 @@ mkProof setup' proof@(PlonkProof cmA cmB cmC cmZ cmT1 cmT2 cmT3 proof1 proof2 a_
   , cmT3'   = convertG1 cmT3
   , proof1' = convertG1 proof1
   , proof2' = convertG1 proof2
-  , a_xi'   = convertF a_xi
-  , b_xi'   = convertF b_xi
-  , c_xi'   = convertF c_xi
-  , s1_xi'  = convertF s1_xi
-  , s2_xi'  = convertF s2_xi
-  , z_xi'   = convertF z_xi
+  , a_xi'   = convertZp a_xi
+  , b_xi'   = convertZp b_xi
+  , c_xi'   = convertZp c_xi
+  , s1_xi'  = convertZp s1_xi
+  , s2_xi'  = convertZp s2_xi
+  , z_xi'   = convertZp z_xi
   , lagsInv = lagrangesInvariants @n setup' proof
   }
 
@@ -87,9 +91,6 @@ challenge ts =
 
 ------------------------------- Base Conversions -------------------------------
 
-convertF :: Fr -> Integer
-convertF = naturalToInteger . fromZp
-
 convertPlonkF :: F -> Fr
 convertPlonkF (F n) = toZp n
 
@@ -98,26 +99,12 @@ convertZp = naturalToInteger . fromZp
 
 -- See CIP-0381 for the conversion specification
 convertG1 :: Point BLS12_381_G1 -> BuiltinByteString
-convertG1 Inf = bls12_381_G1_compressed_zero
-convertG1 (Point x y) = bs
-    where
-        bsX = integerToByteString BigEndian 48 $ convertZp x
-        b   = indexByteString bsX 0
-        b'  = b + 128 + 32 * (if y Haskell.> negate y then 1 else 0)
-        bs  = consByteString b' $ sliceByteString 1 47 bsX
+convertG1 p = let len = BA.length $ toBuiltin $ toStrict $ encode $ compress p
+              in Debug.Trace.trace (Haskell.show len) $ (toBuiltin . toStrict . encode . compress) p
 
 -- See CIP-0381 for the conversion specification
 convertG2 :: Point BLS12_381_G2 -> BuiltinByteString
-convertG2 Inf = bls12_381_G2_compressed_zero
-convertG2 (Point x y) = bs
-    where
-        f (Ext2 a0 a1) = integerToByteString BigEndian 48 (convertZp a1) <> integerToByteString BigEndian 48 (convertZp a0)
-        bsX  = f x
-        bsY  = f y
-        bsY' = f $ negate y
-        b   = indexByteString bsX 0
-        b'  = b + 128 + 32 * (if bsY `greaterThanByteString` bsY' then 1 else 0)
-        bs  = consByteString b' $ sliceByteString 1 95 bsX
+convertG2 = toBuiltin . toStrict . encode . compress
 
 ------------------ Transcript for NonInteractiveProof Plonk32 ------------------
 
