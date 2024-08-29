26f6af38e186818e48b42970c616d91454baab5a
charrsky <maxchrlv@gmail.com>
2024-08-29 17:51:28 +0300

    make plonkVerifier benchmark work

--- benchs/bench-cpu-mem/Main.hs
+++ benchs/bench-cpu-mem/Main.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 
 import           Bench.Scripts                               (plonkVerifierScript, symbolicVerifierScript, verifyPlonkScript)
 import           Bench.Statistics                            (TestSize (..), printHeader, printSizeStatistics)
-import           PlutusLedgerApi.V3                          
+import           PlutusLedgerApi.V3
 import           Prelude                                     hiding (Bool, Eq (..), Fractional (..), Num (..), length)
 import           System.IO                                   (Handle, stdout)
 import           Text.Printf                                 (hPrintf)
@@ -15,9 +16,12 @@ import           Test.QuickCheck.Arbitrary                   (Arbitrary (..))
 import           Test.QuickCheck.Gen                         (generate)
 import           ZkFold.Cardano.Examples.EqualityCheck       (equalityCheckVerificationBytes)
 import qualified PlutusLedgerApi.V2 as V2
-import qualified PlutusLedgerApi.V3 as V3
 import qualified Data.Maybe as Haskell
-
+import Flat.Types ()
+import Data.String ( IsString(fromString) )
+import qualified Data.ByteString as BS
+import ZkFold.Cardano.Plonk.OnChain.Data (ProofBytes(..))
+import qualified ZkFold.Cardano.Plonk.OnChain.BLS12_381.F as F
 
 contextPlonk :: ScriptContext
 contextPlonk = ScriptContext
@@ -25,24 +29,29 @@ contextPlonk = ScriptContext
     { txInfoInputs = []                     :: [TxInInfo]
     , txInfoReferenceInputs = []            :: [TxInInfo]
     , txInfoOutputs = []                    :: [V2.TxOut]
-    , txInfoFee = _                         :: V2.Lovelace
-    , txInfoMint = _                        :: V2.Value
-    , txInfoTxCerts = []                    :: [TxCert]
-    , txInfoWdrl = _                        :: Map V2.Credential V2.Lovelace
-    , txInfoValidRange = always             :: V2.POSIXTimeRange
-    , txInfoSignatories = []                :: [V2.PubKeyHash]
-    , txInfoRedeemers = _                   :: Map ScriptPurpose V2.Redeemer
-    , txInfoData = _                        :: Map V2.DatumHash V2.Datum
-    , txInfoId = _                          :: V3.TxId
-    , txInfoVotes = _                       :: Map Voter (Map GovernanceActionId Vote)
-    , txInfoProposalProcedures = []         :: [ProposalProcedure]
+    , txInfoFee = V2.Lovelace 0
+    , txInfoMint = Value $ unsafeFromList [(CurrencySymbol (toBuiltin (fromString "cursymbol" :: BS.ByteString)), unsafeFromList [(TokenName (toBuiltin (fromString "token" :: BS.ByteString)), 1)])]
+    , txInfoTxCerts = []
+    , txInfoWdrl = unsafeFromList []
+    , txInfoValidRange = always
+    , txInfoSignatories = []
+    , txInfoRedeemers = unsafeFromList [(Spending (TxOutRef (fromString "00") 0), Redeemer (toBuiltinData dummyRedeemer))]
+    , txInfoData = unsafeFromList []
+    , txInfoId = fromString "00" :: TxId
+    , txInfoVotes = unsafeFromList []
+    , txInfoProposalProcedures = []
     , txInfoCurrentTreasuryAmount = Nothing :: Haskell.Maybe V2.Lovelace
     , txInfoTreasuryDonation = Nothing      :: Haskell.Maybe V2.Lovelace
     },
-    scriptContextRedeemer = Redeemer _,
-    scriptContextScriptInfo = MintingScript _
+    scriptContextRedeemer = Redeemer (toBuiltinData dummyRedeemer),
+    scriptContextScriptInfo = SpendingScript (TxOutRef (fromString "00") 0) (Just (Datum (toBuiltinData (0 :: Integer))))
   }
 
+dummyRedeemer :: ProofBytes
+dummyRedeemer = ProofBytes e e e e e e e e e 0 0 0 0 0 0 (F.F 0)
+  where e = ""
+
+{-
 contextSymbolic :: ScriptContext
 contextSymbolic = ScriptContext
   { scriptContextTxInfo = TxInfo
@@ -66,6 +75,7 @@ contextSymbolic = ScriptContext
     scriptContextRedeemer = Redeemer _,
     scriptContextScriptInfo = RewardingScript _
   }
+-}
 
 printCostsSymbolicVerifier :: Handle -> SetupVerify PlonkPlutus -> Proof PlonkPlutus -> ScriptContext -> IO ()
 printCostsSymbolicVerifier h s p ctx = printSizeStatistics h NoSize (symbolicVerifierScript s p ctx)
@@ -82,8 +92,6 @@ main = do
     x           <- generate arbitrary
     ps          <- generate arbitrary
     targetValue <- generate arbitrary
-    
-    putStr $ "x: " ++ show x ++ "\n" ++ "ps: " ++ show ps ++ "\n" ++ "targetValue: " ++ show targetValue ++ "\n"
 
     let (setup, input, proof) = equalityCheckVerificationBytes x ps targetValue
         h = stdout
@@ -101,6 +109,6 @@ main = do
     hPrintf h "\n\n"
     hPrintf h "Run symbolic plonk verifier\n\n"
     printHeader h
-    printCostsSymbolicVerifier h setup proof contextSymbolic
+    -- printCostsSymbolicVerifier h setup proof contextSymbolic
     hPrintf h "\n\n"
 
