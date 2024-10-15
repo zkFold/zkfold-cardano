42971c2a064cedce43b41d231e8593674f5d6ddb
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-10-15 19:54:05 +0200

    Cleanup

--- test/Main.hs
+++ test/Main.hs
@@ -2,14 +2,7 @@ module Main where
 
 import           Prelude
 import           Tests.Compatibility (specCompatibility)
-import           Test.QuickCheck
-
-import           ZkFold.Cardano.Examples.EmptyCircuit
 
 main :: IO ()
 main = do
-    x <- generate arbitrary
-    ps <- generate arbitrary
-    print emptyCircuit
-    print $ testEmptyCircuit x ps
     specCompatibility
