7ca602ffa4adac3bd298e38448afb21c6d866bc2
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2024-10-15 19:53:36 +0200

    Add `EmptyCircuit`

--- test/Main.hs
+++ test/Main.hs
@@ -2,7 +2,14 @@ module Main where
 
 import           Prelude
 import           Tests.Compatibility (specCompatibility)
+import           Test.QuickCheck
+
+import           ZkFold.Cardano.Examples.EmptyCircuit
 
 main :: IO ()
 main = do
+    x <- generate arbitrary
+    ps <- generate arbitrary
+    print emptyCircuit
+    print $ testEmptyCircuit x ps
     specCompatibility
