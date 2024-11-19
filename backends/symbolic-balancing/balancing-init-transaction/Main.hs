b3f5b04c2437c8fee59b102087dd3ae034807cd4
ajuggler <ajuggler@users.noreply.github.com>
2024-11-19 12:54:04 -0600

    Minor update.

--- backends/symbolic-balancing/balancing-init-transaction/Main.hs
+++ backends/symbolic-balancing/balancing-init-transaction/Main.hs
@@ -3,31 +3,24 @@ module Main where
 import           Cardano.Api                              (IsPlutusScriptLanguage, PlutusScriptV3,
                                                            writeFileTextEnvelope)
 import           Cardano.Api.Ledger                       (toCBOR)
-import           Cardano.Api.Shelley                      (File (..), PlutusScript (..), fromPlutusData,
-                                                           scriptDataFromJsonDetailedSchema)
+import           Cardano.Api.Shelley                      (File(..), PlutusScript(..), fromPlutusData)
 import           Codec.CBOR.Write                         (toStrictByteString)
 import           Control.Monad                            (void)
-import           Data.Aeson                               (decode, encode)
+import           Data.Aeson                               (encode)
 import qualified Data.ByteString                          as BS
 import qualified Data.ByteString.Lazy                     as BL
-import           Data.Maybe                               (fromJust)
 import qualified PlutusLedgerApi.V3                       as V3
-import           PlutusTx                                 (CompiledCode, ToData (..))
-import           PlutusTx.Builtins.Internal               (serialiseData)
-import           PlutusTx.Prelude                         (blake2b_224, head)
-import           Prelude                                  (Either (..), FilePath, IO, Maybe (..), Show (..), putStr,
-                                                           ($), (++), (.), (<$>))
+import           PlutusTx                                 (CompiledCode, ToData(..))
+import           Prelude                                  (Bool(..), FilePath, IO, Maybe(..), Show(..), putStr,
+                                                           ($), (++), (.))
 import           System.Directory                         (createDirectoryIfMissing, getCurrentDirectory)
 import           System.FilePath                          (takeFileName, (</>))
-import           Test.QuickCheck.Arbitrary                (Arbitrary (..))
+import           Test.QuickCheck.Arbitrary                (Arbitrary(..))
 import           Test.QuickCheck.Gen                      (generate)
 
-import           ZkFold.Base.Protocol.NonInteractiveProof (HaskellCore, NonInteractiveProof (..))
-import           ZkFold.Cardano.Examples.IdentityCircuit  (stateCheckVerificationBytes)
-import           ZkFold.Cardano.OffChain.E2E              (IdentityCircuitContract (..))
-import           ZkFold.Cardano.OnChain.BLS12_381         (F (..), toInput)
-import           ZkFold.Cardano.OnChain.Plonk             (PlonkPlutus)
-import           ZkFold.Cardano.UPLC                      (parkingSpotCompiled, symbolicVerifierCompiled')
+import           ZkFold.Cardano.Examples.IdentityCircuit  (identityCircuitVerificationBytes)
+import           ZkFold.Cardano.OffChain.E2E              (IdentityCircuitContract(..))
+import           ZkFold.Cardano.UPLC                      (symbolicVerifierCompiled', parkingSpotCompiled)
 
 
 writePlutusScriptToFile :: IsPlutusScriptLanguage lang => FilePath -> PlutusScript lang -> IO ()
@@ -62,40 +55,12 @@ main = do
 
   putStr $ "x: " ++ show x ++ "\n" ++ "ps: " ++ show ps ++ "\n\n"
 
-  let input             = F 26217937587563095239723870254092982918845276250263818911301829349969290592256  -- an arbitrary value
-  let (setup, _, proof) = stateCheckVerificationBytes x ps input
+  -- let input         = F 26217937587563095239723870254092982918845276250263818911301829349969290592256  -- an arbitrary value
+  let (setup, _, _) = identityCircuitVerificationBytes x ps
 
   let assetsPath = path </> "assets"
 
   savePlutus (assetsPath </> "symbolic.plutus") $ symbolicVerifierCompiled' setup
-
   savePlutus (assetsPath </> "parkingSpot.plutus") $ parkingSpotCompiled 54
 
   BS.writeFile (assetsPath </> "unit.cbor") $ dataToCBOR ()
-
-{-
-
-  let result = show $ verify @PlonkPlutus @HaskellCore setup input proof
-  putStr $ "Result: " ++ result ++ ".\n\n"
-
-  datumExampleE <- scriptDataFromJsonDetailedSchema . fromJust . decode <$> BL.readFile "datumExample.json"
-
-  putStr $ (show datumExampleE) ++ "\n\n"
-
-  txin <- parseJsonToTxInInfoList <$> BL.readFile "utxoExample.json"
-
-  putStr $ (show txin) ++ "\n\n"
-
-  case txin of
-    Right txins -> do
-      let txinBD  = toBuiltinData . head $ txins
-      putStr $ "Data: " ++ (show txinBD) ++ "\n\n"
-
-      let txinBBS = serialiseData txinBD
-      putStr $ "Serialised data: " ++ (show txinBBS) ++ "\n\n"
-
-      let input' = toInput $ blake2b_224 txinBBS
-      putStr $ "Input: " ++ (show input') ++ "\n\n"
-
-    Left errMsg -> putStr $ "Error: " ++ errMsg ++ "\n\n"
--}
