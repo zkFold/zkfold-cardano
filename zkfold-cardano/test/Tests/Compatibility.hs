f5cf8f28f83f0fdfd1fdd257059af6827cf1644d
Ilia Baryshnikov <zlonast3@gmail.com>
2025-01-15 23:47:36 +0300

    fix instance Show1

--- zkfold-cardano/test/Tests/Compatibility.hs
+++ zkfold-cardano/test/Tests/Compatibility.hs
@@ -3,7 +3,6 @@
 
 module Tests.Compatibility (specCompatibility) where
 
-import           Data.Functor.Classes                     (Show1 (..))
 import           GHC.Base                                 (Void)
 import           GHC.Generics
 import           Prelude                                  hiding (Eq (..), Fractional (..), Num (..), length)
@@ -19,18 +18,6 @@ import           ZkFold.Cardano.OnChain.Plonkup           (PlonkupPlutus)
 propCompatibility :: forall a b . CompatibleNonInteractiveProofs a b HaskellCore =>
     (a, Witness a) -> Bool
 propCompatibility (a, w) = nipCompatibility @a @b @HaskellCore a w
-
-instance Show1 U1 where
-  liftShowsPrec _ _ = showsPrec
-
-instance Show1 (U1 :*: U1) where
-  liftShowsPrec _ _ = showsPrec
-
-instance Show1 Par1 where
-  liftShowsPrec f _ _ (Par1 x) = f 0 x
-
-instance Show1 (Par1 :*: U1) where
-  liftShowsPrec f _ _ (Par1 x :*: U1) = f 0 x
 
 instance Arbitrary (U1 a) where
   arbitrary = return U1
