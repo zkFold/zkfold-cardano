83727ffab66d4e551aa2d4c503da7c2e85bd9a75
zlonast <zlonast3@gmail.com>
2025-01-22 09:50:33 +0000

    refactor EqualityCheck

--- zkfold-cardano-scripts/bench/bench-cpu-mem/Main.hs
+++ zkfold-cardano-scripts/bench/bench-cpu-mem/Main.hs
@@ -35,13 +35,23 @@ import qualified UntypedPlutusCore                        as UPLC
 import           UntypedPlutusCore                        (UnrestrictedProgram (..))
 
 import           ZkFold.Base.Protocol.NonInteractiveProof (HaskellCore, NonInteractiveProof (..))
-import           ZkFold.Cardano.Examples.EqualityCheck    (equalityCheckVerificationBytes)
+import           ZkFold.Cardano.Examples.EqualityCheck    (equalityCheckContract)
 import           ZkFold.Cardano.OffChain.Utils            (savePlutus)
 import qualified ZkFold.Cardano.OnChain.BLS12_381.F       as F
 import           ZkFold.Cardano.OnChain.Plonkup           (PlonkupPlutus)
 import           ZkFold.Cardano.OnChain.Plonkup.Data      (InputBytes, ProofBytes (..), SetupBytes)
 import           ZkFold.Cardano.UPLC.PlonkVerifierToken   (plonkVerifierTokenCompiled)
 import           ZkFold.Cardano.UPLC.PlonkVerifierTx      (plonkVerifierTxCompiled)
+import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1, Fr)
+import           ZkFold.Base.Protocol.Plonkup                (Plonkup (..))
+import           ZkFold.Base.Protocol.Plonkup.Prover.Secret  (PlonkupProverSecret)
+import           ZkFold.Base.Protocol.Plonkup.Utils          (getParams)
+import           ZkFold.Base.Protocol.Plonkup.Witness        (PlonkupWitnessInput (..))
+import           ZkFold.Cardano.OffChain.Plonkup             (PlonkupN, mkInput, mkProof, mkSetup)
+import           ZkFold.Cardano.OnChain.Plonkup              ()
+import           ZkFold.Symbolic.Compiler                    (ArithmeticCircuit (..))
+import qualified ZkFold.Symbolic.Compiler as ZK
+import           GHC.Generics                                (Par1 (..), U1 (..), type (:*:) (..))
 
 
 contextPlonk :: ProofBytes -> ScriptContext
@@ -154,6 +164,19 @@ plonkVerifierCompiled computation =
     $$(compile [|| untypedPlonkVerifier ||])
     `unsafeApplyCode` liftCodeDef computation
 
+equalityCheckVerificationBytes :: Fr -> PlonkupProverSecret BLS12_381_G1 -> Fr -> (SetupBytes, InputBytes, ProofBytes)
+equalityCheckVerificationBytes x ps targetValue =
+    let ac = ZK.compile @Fr (equalityCheckContract @Fr @(ArithmeticCircuit Fr (U1 :*: U1) (Par1 :*: U1)) targetValue) :: ArithmeticCircuit Fr (U1 :*: U1) (Par1 :*: U1) Par1
+
+        (omega, k1, k2) = getParams 32
+        witnessInputs   = Par1 targetValue :*: U1
+        plonkup = Plonkup omega k1 k2 ac x :: PlonkupN (U1 :*: U1) (Par1 :*: U1) 32
+        setupP  = setupProve @_ @HaskellCore plonkup
+        setupV  = setupVerify @_ @HaskellCore plonkup
+        witness = (PlonkupWitnessInput @_ @(Par1 :*: U1) @_ (U1 :*: U1) witnessInputs, ps)
+        (input, proof) = prove @(PlonkupN (U1 :*: U1) (Par1 :*: U1) 32) @HaskellCore setupP witness
+
+    in (mkSetup setupV, mkInput input, mkProof proof)
 
 main :: IO ()
 main = do
--- zkfold-example/src/ZkFold/Cardano/Examples/EqualityCheck.hs
+++ zkfold-example/src/ZkFold/Cardano/Examples/EqualityCheck.hs
@@ -1,45 +1,12 @@
 module ZkFold.Cardano.Examples.EqualityCheck where
 
-import           Data.Aeson                                  (FromJSON, ToJSON)
-import           GHC.Generics                                (Generic, Par1 (..), U1 (..), type (:*:) (..))
 import           Prelude                                     hiding (Bool, Eq (..), Fractional (..), Num (..), length)
 
 import           ZkFold.Base.Algebra.Basic.Class             (FromConstant (..))
-import           ZkFold.Base.Algebra.EllipticCurve.BLS12_381 (BLS12_381_G1, Fr)
-import           ZkFold.Base.Protocol.NonInteractiveProof    (HaskellCore, NonInteractiveProof (..))
-import           ZkFold.Base.Protocol.Plonkup                (Plonkup (..))
-import           ZkFold.Base.Protocol.Plonkup.Prover.Secret  (PlonkupProverSecret)
-import           ZkFold.Base.Protocol.Plonkup.Utils          (getParams)
-import           ZkFold.Base.Protocol.Plonkup.Witness        (PlonkupWitnessInput (..))
-import           ZkFold.Cardano.OffChain.Plonkup             (PlonkupN, mkInput, mkProof, mkSetup)
-import           ZkFold.Cardano.OnChain.Plonkup              ()
-import           ZkFold.Cardano.OnChain.Plonkup.Data         (InputBytes, ProofBytes, SetupBytes)
 import           ZkFold.Symbolic.Class                       (Symbolic (..))
-import           ZkFold.Symbolic.Compiler                    (ArithmeticCircuit (..), compile)
 import           ZkFold.Symbolic.Data.Bool                   (Bool (..))
 import           ZkFold.Symbolic.Data.Eq                     (Eq (..))
 import           ZkFold.Symbolic.Data.FieldElement           (FieldElement)
 
-data EqualityCheckContract = EqualityCheckContract {
-    x           :: Fr
-  , ps          :: PlonkupProverSecret BLS12_381_G1
-  , targetValue :: Fr
-} deriving stock (Show, Generic)
-  deriving anyclass (ToJSON, FromJSON)
-
 equalityCheckContract :: forall a c . (Symbolic c, FromConstant a (BaseField c)) => a -> FieldElement c -> Bool c
 equalityCheckContract targetValue inputValue = inputValue == fromConstant targetValue
-
-equalityCheckVerificationBytes :: Fr -> PlonkupProverSecret BLS12_381_G1 -> Fr -> (SetupBytes, InputBytes, ProofBytes)
-equalityCheckVerificationBytes x ps targetValue =
-    let ac = compile @Fr (equalityCheckContract @Fr @(ArithmeticCircuit Fr (U1 :*: U1) (Par1 :*: U1)) targetValue) :: ArithmeticCircuit Fr (U1 :*: U1) (Par1 :*: U1) Par1
-
-        (omega, k1, k2) = getParams 32
-        witnessInputs   = Par1 targetValue :*: U1
-        plonkup = Plonkup omega k1 k2 ac x :: PlonkupN (U1 :*: U1) (Par1 :*: U1) 32
-        setupP  = setupProve @_ @HaskellCore plonkup
-        setupV  = setupVerify @_ @HaskellCore plonkup
-        witness = (PlonkupWitnessInput @_ @(Par1 :*: U1) @_ (U1 :*: U1) witnessInputs, ps)
-        (input, proof) = prove @(PlonkupN (U1 :*: U1) (Par1 :*: U1) 32) @HaskellCore setupP witness
-
-    in (mkSetup setupV, mkInput input, mkProof proof)
