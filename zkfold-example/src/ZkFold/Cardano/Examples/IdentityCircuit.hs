828431da6199de0549548d43f55cc93677d6a949
Vladimir <me.morozovvp@gmail.com>
2025-03-03 18:59:31 +1000

    WIP redeemer constructor

--- zkfold-example/src/ZkFold/Cardano/Examples/IdentityCircuit.hs
+++ zkfold-example/src/ZkFold/Cardano/Examples/IdentityCircuit.hs
@@ -30,14 +30,14 @@ identityCircuit = idCircuit
 
 identityCircuitVerificationBytes :: Fr -> PlonkupProverSecret BLS12_381_G1_Point -> (SetupBytes, InputBytes, ProofBytes)
 identityCircuitVerificationBytes x ps =
-    let (omega, k1, k2) = getParams 2
+    let (omega, k1, k2) = getParams 16384
         witnessInputs   = eval identityCircuit (U1 :*: U1) $ Par1 zero
         (gs, h1)        = getSecrectParams x
         plonkup         = Plonkup omega k1 k2 identityCircuit h1 gs :: PlonkupN (U1 :*: U1) Par1 2
         setupP          = setupProve @_ @HaskellCore plonkup
         setupV          = setupVerify @_ @HaskellCore plonkup
         witness         = (PlonkupWitnessInput @_ @_ @BLS12_381_G1_Point (U1 :*: U1) witnessInputs, ps)
-        (input, proof)  = prove @(PlonkupN (U1 :*: U1) Par1 2) @HaskellCore setupP witness
+        (input, proof)  = prove @(PlonkupN (U1 :*: U1) Par1 16384) @HaskellCore setupP witness
 
     in (mkSetup setupV, mkInput input, mkProof proof)
 
@@ -45,18 +45,22 @@ stateCheckVerificationBytes :: Fr -> PlonkupProverSecret BLS12_381_G1_Point -> F
 stateCheckVerificationBytes x ps state =
     let F n             = state
         state'          = toZp n :: Fr
-        (omega, k1, k2) = getParams 2
-        (gs, h1) = getSecrectParams @2 @BLS12_381_G1_Point @BLS12_381_G2_Point x
+        (omega, k1, k2) = getParams 16384
+        (gs, h1) = getSecrectParams @16384 @BLS12_381_G1_Point @BLS12_381_G2_Point x
         witnessInputs   = Par1 state'
+<<<<<<< HEAD
 <<<<<<< HEAD
         (gs, h1)        = getSecrectParams x
 =======
 >>>>>>> 0f58e9d (Fixed errors; using mock zkLogin)
         plonkup         = Plonkup omega k1 k2 identityCircuit h1 gs :: PlonkupN (U1 :*: U1) Par1 2
+=======
+        plonkup         = Plonkup omega k1 k2 identityCircuit h1 gs :: PlonkupN (U1 :*: U1) Par1 16384
+>>>>>>> 11a5ece (WIP redeemer constructor)
         setupP          = setupProve @_ @HaskellCore plonkup
         setupV          = setupVerify @_ @HaskellCore plonkup
         witness         = (PlonkupWitnessInput (U1 :*: U1) witnessInputs, ps)
-        (input, proof)  = prove @(PlonkupN (U1 :*: U1) Par1 2) @HaskellCore setupP witness
+        (input, proof)  = prove @(PlonkupN (U1 :*: U1) Par1 16384) @HaskellCore setupP witness
 
     in (mkSetup setupV, mkInput input, mkProof proof)
 
