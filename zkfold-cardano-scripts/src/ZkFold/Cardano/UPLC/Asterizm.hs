d2551a1201c9f1d36cd2a93abdd372cb14eb159f
Vladimir Sinyakov <vladimir.siniakov@gmail.com>
2025-03-27 11:32:57 +0100

    Add Asterizm messaging script

---
+++ zkfold-cardano-scripts/src/ZkFold/Cardano/UPLC/Asterizm.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:profile-all #-}
+{-# OPTIONS_GHC -fplugin-opt PlutusTx.Plugin:conservative-optimisation #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Unused LANGUAGE pragma" #-}
+
+module ZkFold.Cardano.UPLC.Asterizm where
+
+import           PlutusTx                                 (CompiledCode, compile)
+import qualified PlutusTx.Builtins.Internal               as BI
+import           PlutusTx.Prelude                         (Bool (..), BuiltinData, BuiltinUnit, Ord (..), check, ($),
+                                                           (&&), (.), (||), (<>), (==))
+
+-- | Plutus script (minting policy) for posting signed messages on-chain.
+{-# INLINABLE untypedAsterizmMessage #-}
+untypedAsterizmMessage :: BuiltinData -> BuiltinUnit
+untypedAsterizmMessage ctx =
+  check $ eqCs && (conditionBurning || conditionVerifying)
+  where
+      scriptContextTxInfo'     = BI.snd $ BI.unsafeDataAsConstr ctx
+      scriptContextRedeemer'   = BI.tail scriptContextTxInfo'
+      scriptContextScriptInfo' = BI.tail scriptContextRedeemer'
+
+      cs'        = BI.head $ BI.snd $ BI.unsafeDataAsConstr $ BI.head scriptContextScriptInfo'
+      info       = BI.head scriptContextTxInfo'
+      txInfoMint = BI.head $ tail4 $ BI.snd $ BI.unsafeDataAsConstr info
+      tail4      = BI.tail . BI.tail . BI.tail . BI.tail
+
+      mints' = BI.head $ BI.unsafeDataAsMap txInfoMint
+      eqCs   = BI.ifThenElse (BI.equalsData cs' $ BI.fst mints') True False
+      m'     = BI.head $ BI.unsafeDataAsMap $ BI.snd mints'
+
+      t = BI.unsafeDataAsB $ BI.fst m'
+      n = BI.unsafeDataAsI $ BI.snd m'
+
+      -- Extract message from ScriptContext
+      message = BI.unsafeDataAsB $ BI.head scriptContextRedeemer'
+      -- Hash message
+      hash    = BI.sha2_256 message
+
+      -- First signatory of the transaction
+      vk = BI.unsafeDataAsB $ BI.head $ BI.unsafeDataAsList $ BI.head $ tail4 $ tail4 $ BI.snd $ BI.unsafeDataAsConstr info
+
+      -- Burning already minted tokens
+      conditionBurning = n < 0
+
+      -- Verifying the Plonkup `proof` for the `computation` on `input`
+      conditionVerifying = t == vk <> hash
+
+asterizmMessageCompiled :: CompiledCode (BuiltinData -> BuiltinUnit)
+asterizmMessageCompiled =
+    $$(compile [|| untypedAsterizmMessage ||])
